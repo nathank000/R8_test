9e47dfeadbaf2151962d4be80db2278848f764ee
Nathan King <nathan@betapoint.co>
2023-03-30 16:27:44 -0400

    kill it all start over

--- r8_app/src/View2.js
+++
@@ -1,6 +0,0 @@
-function View2() {
-    return <h1>Hello, View TWO(2)!</h1>;
-  }
-  
-  export default View2;
-  
--- r8_app/src/View3.js
+++
@@ -1,5 +0,0 @@
-function View3() {
-    return <h1>Hello, View THREE(3)!</h1>;
-  }
-  
-  export default View3;
